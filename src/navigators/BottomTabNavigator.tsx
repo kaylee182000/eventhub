c88a068173dd7099edf2b05ec93838b32a3da70d
kaylee182000 <trungkien40414@gmail.com>
2024-02-11 23:43:48 +0700

    update new screen in bottom tab navigator

--- src/navigators/BottomTabNavigator.tsx
+++ src/navigators/BottomTabNavigator.tsx
@@ -1,12 +1,16 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
-import { Home } from '../screens';
+import { Home, Event, AddEvent, Map, Profile } from '../screens';
 
 const BottomTabNavigator = () => {
   const Tab = createBottomTabNavigator();
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Event" component={Event} />
+      <Tab.Screen name="AddEvent" component={AddEvent} />
+      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Profile" component={Profile} />
     </Tab.Navigator>
   );
 };
