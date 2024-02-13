1717860d0317a3fab1d94c9808dad28aeaf05978
kaylee182000 <trungkien40414@gmail.com>
2024-02-13 15:14:06 +0700

    Done bottom tab

--- src/navigators/BottomTabNavigator.tsx
+++ src/navigators/BottomTabNavigator.tsx
@@ -1,11 +1,87 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { Home, Event, AddEvent, Map, Profile } from '../screens';
+import { appColors } from '../constants/appColors';
+import {
+  AddSquare,
+  Calendar,
+  Discover,
+  Location,
+  Profile as ProfileIcon,
+} from 'iconsax-react-native';
+import { CustomText } from '../components';
+import { View } from 'react-native';
 
 const BottomTabNavigator = () => {
   const Tab = createBottomTabNavigator();
+
+  const AddEventView = ({ size }: { size: number }) => {
+    return (
+      <View
+        style={{
+          padding: 16,
+          borderRadius: 50,
+          backgroundColor: appColors.primary,
+          position: 'absolute',
+          top: -20,
+        }}
+      >
+        <AddSquare color={appColors.white} size={30} variant="Bold" />
+      </View>
+    );
+  };
+
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          height: 80,
+          justifyContent: 'center',
+          alignItems: 'center',
+          paddingBottom: 12,
+        },
+        tabBarIcon: ({ color, size, focused }) => {
+          let icon: ReactNode | React.JSX.Element;
+          color = focused ? appColors.primary : appColors.gray;
+          switch (route.name) {
+            case 'Home':
+              icon = <Discover size={size} color={color} variant="Bold" />;
+              break;
+            case 'Event':
+              icon = <Calendar size={size} color={color} variant="Bold" />;
+              break;
+            case 'AddEvent':
+              icon = <AddEventView size={size} />;
+              break;
+            case 'Map':
+              icon = <Location size={size} color={color} variant="Bold" />;
+              break;
+            case 'Profile':
+              icon = <ProfileIcon size={size} color={color} variant="Bold" />;
+              break;
+
+            default:
+              break;
+          }
+
+          return icon;
+        },
+        // tabBarShowLabel: false,
+        tabBarLabel: ({ focused }) => {
+          return (
+            <>
+              {route.name === 'AddEvent' ? null : (
+                <CustomText
+                  text={route.name}
+                  color={focused ? appColors.primary : appColors.gray}
+                />
+              )}
+            </>
+          );
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={Home} />
       <Tab.Screen name="Event" component={Event} />
       <Tab.Screen name="AddEvent" component={AddEvent} />
